e268cd3c797de259ab5fdab2807a009e424f391a
Sarabjeet Singh <sarabjeet05.singh@gmail.com>
2024-06-24 22:56:30 -0400

    binaryTree

---
+++ binaryTree.py
@@ -0,0 +1,51 @@
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def add_child(self, data):
+
+        if data == self.data:
+            return
+
+        if data < self.data:
+
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+
+        else:
+            
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
+
+    def in_order_traversal(self):
+        
+        elements = []
+
+        # visiting the left node
+        if self.left:
+            elements += self.left.in_order_traversal()
+        
+        elements.append(self.data)
+
+        if self.right:
+            elements += self.right.in_order_traversal()
+        
+        return elements
+
+def build_tree(elements):
+    root = BinarySearchTreeNode(elements[0])
+
+    for x in range(1, len(elements)):
+        root.add_child(elements[x])
+    
+    return root
+
+elements = [17, 4, 1, 20, 9, 23, 18, 34]
+tree = build_tree(elements)
+print(tree.in_order_traversal())
