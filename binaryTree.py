6ebfca978b336bc949e9fa9b97d2ef12c11b9c10
Sarabjeet Singh <sarabjeet05.singh@gmail.com>
2024-06-24 23:20:16 -0400

    BinaryTree Updated

--- binaryTree.py
+++ binaryTree.py
@@ -37,6 +37,55 @@ class BinarySearchTreeNode:
             elements += self.right.in_order_traversal()
         
         return elements
+    
+    def search(self, val):
+        if val == self.data:
+            return True
+        
+        if val < self.data:
+            if self.left:
+                self.left.search(val)
+            else:
+                return False
+        
+        if val > self.data:
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
+        
+        return False
+
+    def find_min(self):
+        
+        while self.left:
+            self = self.left
+        
+        return self.data
+    
+    def find_max(self):
+        
+        while self.right:
+            self = self.right
+        
+        return self.data
+
+    def calc_sum(self):
+
+        elements = []
+
+        # visiting the left node
+        if self.left:
+            elements += self.left.in_order_traversal()
+        
+        elements.append(self.data)
+
+        if self.right:
+            elements += self.right.in_order_traversal()
+        
+        return sum(elements)
+
+
 
 def build_tree(elements):
     root = BinarySearchTreeNode(elements[0])
@@ -49,3 +98,7 @@ def build_tree(elements):
 elements = [17, 4, 1, 20, 9, 23, 18, 34]
 tree = build_tree(elements)
 print(tree.in_order_traversal())
+print(tree.search(34))
+print(tree.find_min())
+print(tree.find_max())
+print(tree.calc_sum())
